f850532f26384db89cc1eb89b4e244cff85f01e7
YukiNasumi <ireneenriquez657@gmail.com>
2025-04-30 11:37:47 +0800

    4.30

--- dataAndexample/evaluate.py
+++ dataAndexample/evaluate.py
@@ -5,7 +5,7 @@ base_list = [1, 5, 10, 100]
 for base_num in base_list:
     acc = []
     for i in range(1, 11):
-        fold = np.genfromtxt('experiments/base%d_fold%d.csv' % (base_num, i), delimiter=',', dtype=np.int)
+        fold = np.genfromtxt('experiments/base%d_fold%d.csv' % (base_num, i), delimiter=',', dtype=int)
         accuracy = sum(target[fold[:, 0] - 1] == fold[:, 1]) / fold.shape[0]
         acc.append(accuracy)
 
